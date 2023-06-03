afcde9d39f1274e7bb80ffa312a27ed0304ac181
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-03 14:56:18 +0200

    cvičení 15, konečně se mi teda všechno zobrazilo,i když špatně, přidání ostatních vtip

---
+++ src/Joke/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+const Joke = (props) => {
+  const { userAvatar, userName, text, likes, dislikes } = props;
+  const [upLikes, setUpLikes] = useState(likes);
+  const [downLikes, setDownLikes] = useState(dislikes);
+
+  const handleUpClick = () => {
+    setUpLikes(upLikes + 1);
+  };
+
+  const handleDownClick = () => {
+    setDownLikes(downLikes + 1);
+  };
+  return (
+    <div className="container">
+      <header></header>
+
+      <img src={userAvatar} alt="avatar-user" className="joke__user user-avatar" />
+      <h3 className="user-name">{userName}</h3>
+      <p className="joke__text">{text}</p>
+      <div className="joke__likes">
+        <button onClick={handleUpClick} className="btn-like btn-like--up"></button>
+        <span className="likes-count likes-count--up">{upLikes}</span>
+        <button onClick={handleDownClick} className="btn-like btn-like--down"></button>
+        <span className="likes-count likes-count--down">{downLikes}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Joke;
